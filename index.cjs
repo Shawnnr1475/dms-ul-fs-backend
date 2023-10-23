cc41e4b757829283549f1abc99e1f98f419786bf
Shawnnr1475 <radebeshawnn@gmail.com>
2023-10-23 09:14:03 +0200

    fixing bug

--- index.cjs
+++ index.cjs
@@ -5,7 +5,7 @@ const {createNewDeal} = require("./deals.cjs")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 let dealsToByCreated = []
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(bodyParser.json())
